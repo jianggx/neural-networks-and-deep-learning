a6c986e98964a5f6f942ed2f87db5e9a450ab695
leojiang <guangxiang.jiang@hongpucorp.com>
2018-02-14 09:36:36 +0800

    chapter 1

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -12,6 +12,7 @@ function usually called by our neural network code.
 # Standard library
 import cPickle
 import gzip
+import os.path as path
 
 # Third-party libraries
 import numpy as np
@@ -39,7 +40,7 @@ def load_data():
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
     """
-    f = gzip.open('../data/mnist.pkl.gz', 'rb')
+    f = gzip.open(path.join(path.dirname(__file__),'../data/mnist.pkl.gz'), 'rb')
     training_data, validation_data, test_data = cPickle.load(f)
     f.close()
     return (training_data, validation_data, test_data)
